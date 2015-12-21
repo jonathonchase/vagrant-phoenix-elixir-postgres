7295b640f6fc8af0135280b2474f3c97530c73b9
Krzysztof Wende <iraasta@gmail.com>
2015-12-21 19:41:19 +0100

    function keys fixed

--- config/vagrant/emacs_custom_setup.sh
+++ config/vagrant/emacs_custom_setup.sh
@@ -7,8 +7,7 @@ if [ ! -d /home/vagrant/.emacs.d ]; then
     echo "===== Installing Custom Emacs Config"
     cd /home/vagrant/
     git clone http://github.com/wende/.emacs.d
-    echo "alias emacs=env TERM=xterm-256color emacs -nw" >> /home/vagrant/.bashrc
-    echo "alias emacs=env TERM=xterm-256color emacs -nw" >> /home/vagrant/.zshrc
+    chown vagrant .emacs.d -R
 fi
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/emacs_custom_setup.sh'"
--- config/vagrant/tmux_custom_setup.sh
+++ config/vagrant/tmux_custom_setup.sh
@@ -6,6 +6,7 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/tmux_custom_setup.sh'
 if [ ! -f /home/vagrant/.tmux.conf ]; then
     echo "===== Installing Custom Tmux Config"
     cd /home/vagrant/
+    wget https://raw.githubusercontent.com/plathrop/dotfiles/master/tmux/zenburn.conf
     wget https://gist.githubusercontent.com/wende/fcb97939720c84a6e5c2/raw/4fa21c16707958a333c554a50d2566b6eb935109/.tmux.conf
 fi
 
