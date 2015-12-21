6c1961d54153610fa9385f7fd38fef18a972b04d
Krzysztof Wende <iraasta@gmail.com>
2015-12-21 19:49:29 +0100

    fixes

--- config/vagrant/zsh_custom_setup.sh
+++ config/vagrant/zsh_custom_setup.sh
@@ -6,7 +6,7 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/zsh_custom_setup.sh'"
 if [ -z `which zsh` ]; then
     echo "===== Installing Custom ZSH Config"
     sudo apt-get install -y zsh
-    wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     sudo chsh -s /bin/zsh vagrant
     cd /home/vagrant/
     wget https://gist.githubusercontent.com/anonymous/de9426459b2ab651eff0/raw/216fc2d805f5df906eeaeee8e46e75154f6805c2/.zshrc
