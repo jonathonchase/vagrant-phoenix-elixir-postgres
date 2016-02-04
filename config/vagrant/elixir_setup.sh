a458d2bdfce8877451b8f5ef90982261daf84b68
Krzysztof Wende <iraasta@gmail.com>
2016-02-04 18:08:38 +0100

    node and phoenix

--- config/vagrant/elixir_setup.sh
+++ config/vagrant/elixir_setup.sh
@@ -5,10 +5,14 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/elixir_setup.sh'"
 
 # Install Git if not available
 if [ -z `which elixir` ]; then
-  echo "===== Installing Elixir"
-  wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
-  apt-get -y update
-  apt-get -y install elixir
+    echo "===== Installing Elixir"
+    sudo apt-get install inotify-tools
+    wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+    wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
+    sudo apt-key add erlang_solutions.asc
+    apt-get -y update
+    sudo apt-get install esl-erlang
+    apt-get -y install elixir
 fi
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/elixir_setup.sh'"
--- config/vagrant/nodejs_setup.sh
+++ config/vagrant/nodejs_setup.sh
@@ -5,7 +5,7 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/nodejs_setup.sh'"
 if [ -z `which nodejs` ]; then
   # Instructions from:
   # https://nodesource.com/blog/nodejs-v012-iojs-and-the-nodesource-linux-repositories
-  curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
   sudo apt-get install -y nodejs
 fi
 
