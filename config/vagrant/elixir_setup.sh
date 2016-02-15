32295b45acacc22278caed4d54c883cd6ec0d4ed
Krzysztof Wende <iraasta@gmail.com>
2016-02-15 10:44:26 +0100

    elixir fix

--- config/vagrant/elixir_setup.sh
+++ config/vagrant/elixir_setup.sh
@@ -6,13 +6,14 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/elixir_setup.sh'"
 # Install Git if not available
 if [ -z `which elixir` ]; then
     echo "===== Installing Elixir"
-    yes Y | sudo apt-get install inotify-tools
+    sudo apt-get install -y inotify-tools
     wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
     wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
-    yes Y | sudo apt-key add erlang_solutions.asc
-    yes Y | apt-get -y update
-    yes Y | sudo apt-get install esl-erlang
-    yes Y | sudo apt-get -y install elixir
+    sudo apt-key add erlang_solutions.asc
+    sudo apt-get -y update
+    sudo apt-get -f install
+    sudo apt-get -y install esl-erlang
+    sudo apt-get -y install elixir
 fi
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/elixir_setup.sh'"
