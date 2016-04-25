e3b4ec2e4d69b852a36bdf5053db6394fc7e4665
Krzysztof Wende <chris@neontree.pl>
2016-04-25 16:53:17 +0200

    gitlab runner

---
+++ config/vagrant/gitlab_multi_runner_setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "=== Begin Gitlab Multi Runner Provisioning using 'config/vagrant/gitlab_multi_runner_setup.sh'"
+
+# Install Git if not available
+if [ -z "$GITLAB_URL" ] && [ -z "$GITLAB_TOKEN" ]; then
+    echo "===== Installing GitLab MutliRunner ====="
+    curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
+    sudo apt-get install -y gitlab-ci-multi-runner
+    sudo gitlab-ci-multi-runner register -u $GITLAB_URL -t $GITLAB_TOKEN --executor shell -n --tag-list elixir,phoenix,erlang,postgresql
+fi
+
+echo "=== End Vagrant Provisioning using 'config/vagrant/gitlab_multi_runner_setup.sh'"
