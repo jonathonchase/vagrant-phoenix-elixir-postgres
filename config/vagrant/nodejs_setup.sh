f9be8cae542fce5969c9f399230397594e06e481
Krzysztof Wende <chris@neontree.pl>
2016-04-26 20:06:03 +0200

    new mix ecto.setup

--- config/vagrant/nodejs_setup.sh
+++ config/vagrant/nodejs_setup.sh
@@ -7,6 +7,7 @@ if [ -z `which nodejs` ]; then
     # https://nodesource.com/blog/nodejs-v012-iojs-and-the-nodesource-linux-repositories
     curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
     sudo apt-get install -y nodejs
+    npm install brunch -g
 fi
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/nodejs_setup.sh'"
